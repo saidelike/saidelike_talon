9aedab904163d12e6d1e1630faff478be3ec4b3d
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2023-06-16 17:12:56 +0100

    fix the vlc player controlling code so it works correctly

--- apps/vlc/vlc.py
+++ apps/vlc/vlc.py
@@ -13,25 +13,17 @@ ctx = Context()
 # if global VLC control is currently enabled or disabled
 vlc_control = False
 
-
-def control_toggle(enable: bool) -> bool:
-    global vlc_control
-    vlc_control = not vlc_control
-    if vlc_control:
-        ctx.tags = ["user.vlc_control"]
-        return True
-    else:
-        ctx.tags = []
-        return False
-
-
 @mod.action_class
 class Actions:
     def vlc_control_toggle(enable: Optional[bool] = None):
         """Toggle enable/disable for the global VLC control"""
-        vlc_control_toggle(enable if enable is not None else True)
-
-
-def vlc_control_toggle(enable: bool):
-    enabled = control_toggle(enable)
-    actions.user.notify(f"Control VLC: {enabled}")
+        global vlc_control
+        if enable is None:
+            vlc_control = not vlc_control
+        else:
+            vlc_control = enable
+        if vlc_control:
+            ctx.tags = ["user.vlc_control"]
+        else:
+            ctx.tags = []
+        actions.user.notify(f"Control VLC: {vlc_control}")
