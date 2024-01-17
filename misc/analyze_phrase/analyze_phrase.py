737cdb00e1ced1b4ea59de3345e454bf9ca724d9
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-01-17 17:04:13 +0000

    fixed due to talon update

--- misc/analyze_phrase/analyze_phrase.py
+++ misc/analyze_phrase/analyze_phrase.py
@@ -139,10 +139,10 @@ def get_captures(capture: Capture) -> AnalyzedCapture:
             name = None
         elif isinstance(c, VMCapture):
             phrase = " ".join(c.unmapped())
-            name = c._name
+            name = c.name
         elif isinstance(c, VMListCapture):
-            phrase = c._value
-            name = c._name
+            phrase = c.value
+            name = c.name
         else:
             raise Exception(f"Unknown capture type '{type(c)}'")
 
